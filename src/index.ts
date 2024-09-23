fd08d120427a797e119861ef704ad827530fe9f4
Mahad Kalam <mahadkalam1@proton.me>
2024-09-23 20:34:24 +0000

    Upload QR code image

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-const { App } = await import('@slack/bolt');
-import { image as qrImage } from "qr-image"
+const {App} = await import('@slack/bolt');
+import {image as qrImage} from "qr-image"
 import 'dotenv/config'
 
 const app = new App({
@@ -7,9 +7,22 @@ const app = new App({
     signingSecret: process.env['SLACK_SIGNING_SECRET']
 });
 
-app.message(async ({ message, say }) => {
-    if (!message.subtype) {
-
+app.message(async ({message}) => {
+    if (!message.subtype && message.text) {
+        const response = await app.client.filesUploadV2({
+            file_uploads: [
+                {
+                    file: qrImage(message.text, {type: 'png'}),
+                    filename: "qr.png",
+                }
+            ],
+            channel_id: message.channel,
+            initial_comment: `Here's your QR code, <@${message.user}>!`,
+            thread_ts: message.thread_ts ?? message.ts,
+        })
+        if (!response.ok || !Array.isArray(response['files'])) {
+            throw new Error(response.error);
+        }
     }
 });
 
